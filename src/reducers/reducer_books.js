7054e18373df830382338442f7905a30b965cadc
somil kumar <somil.kumar91@gmail.com>
2017-10-31 12:17:06 +0530

    Adding repo

--- src/reducers/reducer_books.js
+++
@@ -1,9 +0,0 @@
-export default function() {
-    return [
-        { title: 'Js the gp', pages: 101 },
-        { title: 'Harry', pages: 76 },
-        { title: 'Dark Tower', pages: 22 },
-        { title: 'YDNJS 2', pages: 122 }
-    ]
-}
-
